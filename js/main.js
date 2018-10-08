d0ea93041e6aa0b299ea0161ca6281852406d627
Jenn G <31021774+jennrenee626@users.noreply.github.com>
2018-10-08 14:00:17 -0500

    updated map api token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoianJnNjI2IiwiYSI6ImNqbjBtZzljbTA2Y2EzcG54YWZ1NmVqenMifQ.O6WNFLZfOGtxY6p-loZ-Qw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
