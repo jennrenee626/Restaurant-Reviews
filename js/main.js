5588ccd9710ee9cfbea7fdfb109c9d1463fcda31
Jenn G <31021774+jennrenee626@users.noreply.github.com>
2018-10-09 19:58:37 -0500

    service worker cache

--- js/main.js
+++ js/main.js
@@ -202,33 +202,4 @@ if ('serviceWorker' in navigator) {
           console.log(err);
       });
     };
-
-   let urlsToCache = [
-      '/',
-      '/index.html',
-      '/restaurant.html',
-      '/css/styles.css',
-      '/jss/main.js',
-      '/jss/restaurant_info.js',
-      '/jss/dbhelper.js',
-      '/data/restaurants.json',
-      '/img/1.jpg',
-      '/img/2.jpg',
-      '/img/3.jpg',
-      '/img/4.jpg',
-      '/img/5.jpg',
-      '/img/6.jpg',
-      '/img/7.jpg',
-      '/img/8.jpg',
-      '/img/9.jpg',
-      '/img/10.jpg',
-  ];
     
-  self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('cacheV1')
-        .then(function(cache) {
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
